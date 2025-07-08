b52f1bc13075ebe156c2210da5d0f5b30dcaf8e1
nwillu3993 <69249917+willisleehosan@users.noreply.github.com>
2025-07-08 21:12:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
+import dash
+from dash import html
+
+app = dash.Dash(__name__)
+app.layout = html.Div([
+    html.H1("Hello from Dash on Hugging Face Spaces!")
+])
+
+server = app.server  # required for Hugging Face to recognize the app
+
 if __name__ == "__main__":
-    app.run_server(host='0.0.0.0', port=8080)
+    app.run_server(debug=True, host="0.0.0.0", port=7860)
